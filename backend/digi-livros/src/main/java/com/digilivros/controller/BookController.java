09ffabee1e0b157300d3daf0cbfcf1254ae1e3a2
Vinicius Alexandre <mano19vini@gmail.com>
2020-07-19 17:37:46 -0300

    create delete e put BookController

--- backend/digi-livros/src/main/java/com/digilivros/controller/BookController.java
+++ backend/digi-livros/src/main/java/com/digilivros/controller/BookController.java
@@ -3,13 +3,16 @@ package com.digilivros.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.digilivros.models.Book;
 import com.digilivros.models.Book;
 import com.digilivros.repository.BookRepository;
 
@@ -38,6 +41,14 @@ public class BookController {
 		return bookrepository.save(book);
 	}
 
+	@DeleteMapping("/book")
+	public void  deletBook(@RequestBody Book book) {
+		bookrepository.delete(book);
+	}
 	
+	@PutMapping("/book")
+	public Book  editBook(@RequestBody Book book) {
+		return bookrepository.save(book); 
+	}
 
 }
