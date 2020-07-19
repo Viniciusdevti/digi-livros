665ea53343ffae27b5601be4bc9205aa0f634287
Vinicius Alexandre <mano19vini@gmail.com>
2020-07-19 11:30:33 -0300

    initial commit

---
+++ digi-livros/src/main/java/com/digilivros/digilivros/DigiLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.digilivros.digilivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiLivrosApplication.class, args);
+	}
+
+}
