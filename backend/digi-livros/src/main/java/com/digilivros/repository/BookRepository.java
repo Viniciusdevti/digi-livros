8fcdf177295eebd09c89973f6d5e1b4762f0aace
Vinicius Alexandre <mano19vini@gmail.com>
2020-07-19 17:23:23 -0300

    adicionando informações no Bookrepository

--- backend/digi-livros/src/main/java/com/digilivros/repository/BookRepository.java
+++ backend/digi-livros/src/main/java/com/digilivros/repository/BookRepository.java
@@ -1,5 +1,11 @@
 package com.digilivros.repository;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.digilivros.models.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+	Book findById(long id);
+
 
 }
