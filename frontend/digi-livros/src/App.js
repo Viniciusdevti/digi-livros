09c68c1bbc48723585de17691e822b1cf08f1b1e
Vinicius Alexandre <mano19vini@gmail.com>
2020-07-21 00:30:08 -0300

    criando componetes e adicionando materialize

--- frontend/digi-livros/src/App.js
+++ frontend/digi-livros/src/App.js
@@ -3,20 +3,16 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
     </div>
   );
 }
