6450ce09a8130cef9675b43d4222fcaa23aaa68f
Vinicius Alexandre <mano19vini@gmail.com>
2020-07-21 00:54:00 -0300

    criando header

--- frontend/digi-livros/src/App.js
+++ frontend/digi-livros/src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 
+import Header from "./Header"
+
 function App() {
   return (
     <div className="App">
-       <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
+    
+    <Header></Header>
     </div>
   );
 }
---
+++ frontend/digi-livros/src/Header.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+
+import "./global.css"
+
+export default  function Header() {
+  return (
+    <div>
+    
+<nav>
+<div class="nav-wrapper blue lighten-2">
+  
+  <a href="#" className="brand-logo  "> <i class="material-icons">style</i> Digilivros </a>
+  <ul id="nav-mobile" className="right hide-on-med-and-down ">
+    <li><a  calshref="sass.html ">Clientes</a></li>
+    <li><a href="badges.html">Livros</a></li>
+    <li><a href="collapsible.html">Aluguel</a></li>
+
+ 
+
+
+  </ul>
+</div>
+</nav>
+    </div>
+  );
+}
+
+
+
+
+
